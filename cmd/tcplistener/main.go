9a504b9edc1df62bcc551b360e4b687d6bf4dc30
hassamk122 <hassamkiani66@gmail.com>
2026-02-18 13:07:34 +0500

    removed reading from file func now it reads from network

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,80 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"github.com/hassamk122/http_from_tcp_golang/internal/request"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(out)
-		str := ""
-		for {
-			data := make([]byte, 8)
-			n, err := f.Read(data)
-
-			if n > 0 {
-
-				data = data[:n]
-
-				idxNewline := bytes.IndexByte(data, '\n')
-				if idxNewline != -1 {
-					str += string(data[:idxNewline])
-					data = data[idxNewline+1:]
-					out <- str
-					str = ""
-				}
-
-				str += string(data)
-			}
-
-			if err == io.EOF {
-				break
-			}
-
-			if err != nil {
-				return
-			}
-		}
-
-		if len(str) != 0 {
-			out <- str
-		}
-	}()
-
-	return out
-}
-
-// wrote this for practice
-func getBytesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(out)
-
-		for {
-			data := make([]byte, 8)
-
-			n, err := f.Read(data)
-			if err != nil {
-				break
-			}
-
-			data = data[:n]
-			out <- string(data)
-		}
-
-	}()
-
-	return out
-}
 
 func main() {
 	listener, err := net.Listen("tcp", ":42069")
@@ -83,13 +15,22 @@ func main() {
 	}
 
 	for {
+
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatal("error", "error", err)
 		}
-		for line := range getLinesChannel(conn) {
-			fmt.Printf("read : %s \n", line)
+
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal("error", "error", err)
 		}
+
+		fmt.Printf("Request line: \n")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- version: %s\n", r.RequestLine.HttpVersion)
+
 	}
 
 }
