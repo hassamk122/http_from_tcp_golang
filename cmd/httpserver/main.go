f1f8cb1c42401aad13df08681b1227825ffcadd6
hassamk122 <hassamkiani66@gmail.com>
2026-02-22 16:19:24 +0500

    refactored to writer + serving html

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -14,26 +14,29 @@ import (
 
 const port = 42069
 
-func dummyHandlerfunc(w io.Writer, req *request.Request) *server.HandlerError {
-	switch req.RequestLine.RequestTarget {
-	case "/yourproblem":
-		return &server.HandlerError{
-			StatusCode: response.StatusBadRequest,
-			Message:    "Your problem is not my problem\n",
-		}
-	case "/myproblem":
-		return &server.HandlerError{
-			StatusCode: response.StatusInternalServerError,
-			Message:    "Woopise, my bad\n",
-		}
-	default:
-		w.Write([]byte("All good frr\n"))
+func serveBasicHtml(w *response.Writer, req *request.Request) {
+	h := response.GetDefaultHeaders(0)
+	body := respond200()
+	status := response.StatusOK
+
+	if req.RequestLine.RequestTarget == "/yourproblem" {
+		body = respond400()
+		status = response.StatusBadRequest
+	} else if req.RequestLine.RequestTarget == "/myproblem" {
+		body = respond500()
+		status = response.StatusInternalServerError
 	}
-	return nil
+
+	h.Replace("Content-length", fmt.Sprintf("%d", len(body)))
+	h.Replace("Content-type", "text/html")
+	w.WriteStatusLine(status)
+	w.WriteHeaders(*h)
+	w.WriteBody(body)
+
 }
 
 func main() {
-	s, err := server.Serve(port, dummyHandlerfunc)
+	s, err := server.Serve(port, serveBasicHtml)
 	if err != nil {
 		log.Fatal("Error starting server : %v", err)
 	}
@@ -45,4 +48,46 @@ func main() {
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	<-sigChan
 	log.Println("Server gracefully stopped")
+}
+
+func respond200() []byte {
+	return []byte(`
+<html>
+  <head>
+    <title>200 OK</title>
+  </head>
+  <body>
+    <h1>Success!</h1>
+    <p>Your request was an absolute banger.</p>
+  </body>
+</html>
+	`)
+}
+
+func respond400() []byte {
+	return []byte(`
+	<html>
+  <head>
+    <title>400 Bad Request</title>
+  </head>
+  <body>
+    <h1>Bad Request</h1>
+    <p>Your request honestly kinda sucked.</p>
+  </body>
+</html>
+	`)
+}
+
+func respond500() []byte {
+	return []byte(`
+<html>
+  <head>
+    <title>500 Internal Server Error</title>
+  </head>
+  <body>
+    <h1>Internal Server Error</h1>
+    <p>Okay, you know what? This one is on me.</p>
+  </body>
+</html>
+	`)
 }
